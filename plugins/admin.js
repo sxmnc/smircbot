e3197f2af84e28b25533f8604cc4c3031bb5f758
Alexandre Gagnon <alxgnon@gmail.com>
2014-05-17 17:16:11 -0400

    Add admin plugin

---
+++ plugins/admin.js
@@ -0,0 +1,24 @@
+module.exports = function (bot, core, config) {
+
+  var listener = function (nick, text, msg) {
+    if (text.indexOf('$reqop') == 0) {
+      if (config.operators.indexOf(nick) == -1) {
+        bot.sayPub(nick + ': Permission denied.');
+      } else {
+        bot.send('MODE', core.channel, '+o', nick);
+      }
+    } else if (text.indexOf('$kick ') == 0) {
+      var kickee = text.substring('$kick '.length);
+      if (config.operators.indexOf(nick) == -1) {
+        bot.sayPub(nick + ': Permission denied.');
+      } else {
+        bot.send('KICK', core.channel, kickee);
+      }
+    }
+  }
+  bot.on('pub', listener);
+
+  return function () {
+    bot.removeListener('pub', listener);
+  };
+}
