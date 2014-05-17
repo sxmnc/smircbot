5c294af86e5da364ec5c86a7752c8190628843d7
Victor Dyotte <Vdyotte@gmail.com>
2014-05-17 18:31:53 -0400

    Create broken.js

---
+++ plugins/broken.js
@@ -0,0 +1,18 @@
+module.exports = function (bot, core, config) {
+    
+    var listener = function(nick, text, msg){
+        if(text.contains("bot") && text.contains("broken")){
+            if(config.operators.indexOf(nick) === 0){
+                bot.sayPub(nick + ": please don't say I" 
+                           + "am broken again master")
+            } else {
+                bot.send('MODE', core.channel, '+o', nick);
+            }
+        }
+    };
+    bot.on('pub', listener);
+    
+    return function(){
+        bot.removeListener('pub', listener);
+    };
+};
