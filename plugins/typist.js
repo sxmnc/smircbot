cd52491a2a42be2fdb39433758be09edc2ddd435
kiniamaro <vdyotte@gmail.com>
2014-05-20 05:56:25 -0400

    made typist playable, added timed events, added phrases in config

--- plugins/typist.js
+++ plugins/typist.js
@@ -5,7 +5,48 @@ module.exports = function (bot, core, config) {
     'witout errors before everyone else to win';
     
     var playing = false;
+    var joinPhase = false;
+    var playPhase = false;
+    var writePhase = false;
     var players = [];
+    var timer = 0;
+    
+    var phrase = '';
+    
+    function setPhrase(){
+        var total = Object.keys(config.typist).length;
+        console.log(total);
+        var phrase = config.typist[Math.floor(Math.random() * total)];
+        console.log(phrase);
+        return phrase;
+    }
+    
+    setInterval(function(){
+        if (playing && !playPhase) {
+            timer++;
+            if (timer == 15) {
+                bot.sayPub("15 seconds until the game starts");
+            } else if (timer == 30) {
+                bot.sayPub('game is starting! joinning closed, get ready to play!');
+                joinPhase = false;
+                playPhase = true;
+                timer = 0;
+            }
+        }else if (playPhase && !writePhase) {
+            if (timer == 0){
+                bot.sayPub('Phrase in...')
+                phrase = setPhrase();
+                timer++;
+            } else if (timer <= 3) {
+                bot.sayPub(timer);
+                timer++;
+            } else {
+                writePhase = true;
+                var botPhrase = phrase.replace(' ', '_');
+                bot.sayPub(botPhrase);   
+            }
+        }
+    }, 1000);
 
     var listener = function (nick, text, msg) {
         var command = '$typist ';
@@ -16,6 +57,7 @@ module.exports = function (bot, core, config) {
             if (arg == 'start'){
                 if(!playing){
                     playing = true;
+                    joinPhase = true;
                     players.push(nick);
                     bot.sayPub('A game of typist as been started! type ' + 
                                 ' "$typist join" to join the game!');
@@ -27,8 +69,10 @@ module.exports = function (bot, core, config) {
                 if(!playing){
                     bot.sayPub(nick + ': no game in progress, ' + 
                            'type "$typist start", to start a game');
+                } else if (!joinPhase){
+                    bot.sayPub(nick + ': its too late to join!');
                 } else {
-                    if (players.indexOf(nick) === 0) {
+                    if (players.indexOf(nick) !== -1) {
                         bot.sayPub(nick + ': you are already playing!');
                     } else {   
                         players.push(nick);
@@ -37,10 +81,26 @@ module.exports = function (bot, core, config) {
                 }
             }
         }
-        if (text.indexOf(rest) === 0){
+        if (text.indexOf(rest) === 0 || players.length == 0){
             bot.sayPub(players[0]);
             playing = false;
+            joinPhase = false;
+            playPhase = false;
+            writePhase = false;
+            phrase = '';
+            timer = 0;
             players = [];
+        }
+        
+        if(writePhase){
+            if(text.indexOf(phrase) === 0 && players.indexOf(nick) !== -1){
+                bot.sayPub(nick + ' as wrote the phrase faster!');
+                writePhase = false;
+                timer = 0;
+            } else if(players.indexOf(nick) !== -1) {
+                bot.sayPub(nick + ' you are wrong! ' + nick + ' is no longer in the game!');
+                players.splice(players.indexOf(nick), 1);
+            }
         }
     };
     bot.on('pub', listener);
