151ce6dd6da97ebde765146dbe632dbab15f2c33
Alexandre Gagnon <alxgnon@gmail.com>
2014-05-20 14:50:29 -0400

    plugins/typist code quality control

--- plugins/typist.js
+++ plugins/typist.js
@@ -1,8 +1,8 @@
 module.exports = function (bot, core, config) {
 
     core.help.typist = '$typist\n' +
-    'Starts a typist game, type the sentence\n' +  
-    'witout errors before everyone else to win';
+    'Starts a typist game, type the sentence ' +
+    'without errors before everyone else to win';
     
     var playing = false;
     var joinPhase = false;
@@ -13,11 +13,9 @@ module.exports = function (bot, core, config) {
     
     var phrase = '';
     
-    function setPhrase(){
+    function setPhrase() {
         var total = Object.keys(config.typist).length;
-        console.log(total);
         var phrase = config.typist[Math.floor(Math.random() * total)];
-        console.log(phrase);
         return phrase;
     }
     
@@ -25,16 +23,16 @@ module.exports = function (bot, core, config) {
         if (playing && !playPhase) {
             timer++;
             if (timer == 15) {
-                bot.sayPub("15 seconds until the game starts");
+                bot.sayPub('15 seconds until the game starts');
             } else if (timer == 30) {
-                bot.sayPub('game is starting! joinning closed, get ready to play!');
+                bot.sayPub('game is starting! joining closed, get ready to play!');
                 joinPhase = false;
                 playPhase = true;
                 timer = 0;
             }
-        }else if (playPhase && !writePhase) {
-            if (timer == 0){
-                bot.sayPub('Phrase in...')
+        } else if (playPhase && !writePhase) {
+            if (timer === 0) {
+                bot.sayPub('Phrase in...');
                 phrase = setPhrase();
                 timer++;
             } else if (timer <= 3) {
@@ -50,38 +48,38 @@ module.exports = function (bot, core, config) {
 
     var listener = function (nick, text, msg) {
         var command = '$typist ';
-        var rest = "$reset";
+        var rest = '$reset';
         
         if (text.indexOf(command) === 0) {
             var arg = text.substring(command.length);
-            if (arg == 'start'){
-                if(!playing){
+            if (arg === 'start') {
+                if (!playing) {
                     playing = true;
                     joinPhase = true;
                     players.push(nick);
-                    bot.sayPub('A game of typist as been started! type ' + 
-                                ' "$typist join" to join the game!');
+                    bot.sayPub('A game of typist has been started! type ' + 
+                        ' "$typist join" to join the game!');
                 } else {
                     bot.sayPub(nick + ': A game is already in progress, ' + 
-                           'type "$typist join", to join the game!');
+                        'type "$typist join", to join the game!');
                 }
              } else if (arg == 'join') {
-                if(!playing){
+                if (!playing) {
                     bot.sayPub(nick + ': no game in progress, ' + 
-                           'type "$typist start", to start a game');
-                } else if (!joinPhase){
-                    bot.sayPub(nick + ': its too late to join!');
+                        'type "$typist start", to start a game');
+                } else if (!joinPhase) {
+                    bot.sayPub(nick + ": it's too late to join!");
                 } else {
                     if (players.indexOf(nick) !== -1) {
                         bot.sayPub(nick + ': you are already playing!');
                     } else {   
                         players.push(nick);
-                        bot.sayPub(nick + ': as been added to the game!');
+                        bot.sayPub(nick + ': has been added to the game!');
                     }
                 }
             }
         }
-        if (text.indexOf(rest) === 0 || players.length == 0){
+        if (text.indexOf(rest) === 0 || players.length === 0) {
             bot.sayPub(players[0]);
             playing = false;
             joinPhase = false;
@@ -92,13 +90,14 @@ module.exports = function (bot, core, config) {
             players = [];
         }
         
-        if(writePhase){
-            if(text.indexOf(phrase) === 0 && players.indexOf(nick) !== -1){
-                bot.sayPub(nick + ' as wrote the phrase faster!');
+        if (writePhase) {
+            if (text.indexOf(phrase) === 0 && players.indexOf(nick) !== -1) {
+                bot.sayPub(nick + ' wrote the phrase faster!');
                 writePhase = false;
                 timer = 0;
-            } else if(players.indexOf(nick) !== -1) {
-                bot.sayPub(nick + ' you are wrong! ' + nick + ' is no longer in the game!');
+            } else if (players.indexOf(nick) !== -1) {
+                bot.sayPub(nick + ' you are wrong! ' + nick +
+                    ' is no longer in the game!');
                 players.splice(players.indexOf(nick), 1);
             }
         }
