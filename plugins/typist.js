8fdb43a1912dc30e30830175be68740684f4c205
kiniamaro <vdyotte@gmail.com>
2014-05-22 07:34:18 -0400

    typist: created life system, overall game is now more polished and robust

--- plugins/typist.js
+++ plugins/typist.js
@@ -1,22 +1,43 @@
 module.exports = function (bot, core, config) {
 
     core.help.typist = '$typist\n' +
-    'Starts a typist game, type the sentence ' +
-    'without errors before everyone else to win';
+    'Starts a typist game with "$typist start"\n type the sentence ' +
+    'without errors before everyone else to win points\n' + 
+    'type "$typist reset" to end the current game';
+    
+    var startingLives = 2;
+    var win = 6; // amount of lives you need to win
     
     var playing = false;
     var joinPhase = false;
     var playPhase = false;
     var writePhase = false;
     var players = [];
+    var lives = [];
     var timer = 0;
     
     var phrase = '';
+    
+    function reset(){
+        playing = false;
+        joinPhase = false;
+        playPhase = false;
+        writePhase = false;
+        phrase = '';
+        timer = 0;
+        players = [];
+    }
     
     function setPhrase() {
         var total = Object.keys(config.typist).length;
         var phrase = config.typist[Math.floor(Math.random() * total)];
         return phrase;
+    }
+    
+    function setLives(){
+        for (var i = 0; i < players.length; i++){
+            lives[i] = startingLives;
+        }
     }
     
     setInterval(function(){
@@ -27,6 +48,7 @@ module.exports = function (bot, core, config) {
             } else if (timer == 30) {
                 bot.sayPub('game is starting! joining closed, get ready to play!');
                 joinPhase = false;
+                setLives();
                 playPhase = true;
                 timer = 0;
             }
@@ -36,19 +58,17 @@ module.exports = function (bot, core, config) {
                 phrase = setPhrase();
                 timer++;
             } else if (timer <= 3) {
-                bot.sayPub(timer);
+                bot.sayPub(3 - timer);
                 timer++;
             } else {
                 writePhase = true;
-                var botPhrase = phrase.replace(' ', '_');
-                bot.sayPub(botPhrase);   
+                bot.sayPub(phrase);   
             }
         }
     }, 1000);
 
     var listener = function (nick, text, msg) {
         var command = '$typist ';
-        var rest = '$reset';
         
         if (text.indexOf(command) === 0) {
             var arg = text.substring(command.length);
@@ -77,28 +97,38 @@ module.exports = function (bot, core, config) {
                         bot.sayPub(nick + ': has been added to the game!');
                     }
                 }
-            }
-        }
-        if (text.indexOf(rest) === 0 || players.length === 0) {
-            bot.sayPub(players[0]);
-            playing = false;
-            joinPhase = false;
-            playPhase = false;
-            writePhase = false;
-            phrase = '';
-            timer = 0;
-            players = [];
+            } else if (arg == 'reset' || player.length === 0){
+                reset();
+                bot.sayPub('game as been reset by : ' + nick);
+            }      
         }
         
         if (writePhase) {
             if (text.indexOf(phrase) === 0 && players.indexOf(nick) !== -1) {
-                bot.sayPub(nick + ' wrote the phrase faster!');
+                bot.sayPub(nick + ' wrote the phrase faster! + 1 life to him!');
+                lives[players.indexOf(nick)] += 1;
+                bot.sayPub(nick + ' is now at : ' + lives[players.indexOf(nick)] + ' lives');
+                if (lives[players.indexOf(nick)] >= win) {
+                    bot.sayPub(nick + ' Wins the game!')
+                    reset();
+                }
                 writePhase = false;
                 timer = 0;
             } else if (players.indexOf(nick) !== -1) {
-                bot.sayPub(nick + ' you are wrong! ' + nick +
-                    ' is no longer in the game!');
-                players.splice(players.indexOf(nick), 1);
+                bot.sayPub(nick + ' : you are wrong! ' +
+                    ' you lose 1 life!');
+                lives[players.indexOf(nick)] -= 1;
+                if (lives[players.indexOf(nick)] <= 0) {
+                    players.splice(players.indexOf(nick), 1);
+                    bot.sayPub(nick + ': you are dead!');
+                } else {
+                    bot.sayPub(nick + ': you have ' + lives[players.indexOf(nick)] +
+                               ' live(s) left!' )    
+                }
+            }
+            if (players.length == 0){
+                reset();
+                bot.sayPub('every one is dead! no winner :(')
             }
         }
     };
