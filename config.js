cc6cb954cc8caafca0cfafb62be2086113f0fd87
kiniamaro <vdyotte@gmail.com>
2014-05-28 23:30:59 -0400

    added quotes for quotes.js

--- config.js
+++ config.js
@@ -57,4 +57,11 @@ module.exports = {
     "my precious",
     "I'm the king of the world!",
   ],
+  
+  quotes: [
+    '"C\'est parce que c\'Est pas du loli rape" - kiniamaro 2014',
+    '"ouais, I\'m not letting it die, c\'est mon dernier mac à vie" - alxgnon 2014',
+    'http://i.imgur.com/0CW6W7t.jpg',
+    '"the bot is broken" everyone - 2014'
+  ],
 };
