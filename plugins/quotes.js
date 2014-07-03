df7069fab81a17698ce7580660ac816e8cde5e16
Riboshom <riboshom@hotmail.com>
2014-07-03 18:41:22 -0400

    $quotes now works with keys rather than indexes

--- plugins/quotes.js
+++ plugins/quotes.js
@@ -3,29 +3,47 @@ var _ = require('lodash');
 module.exports = function (core) {
   var plugin = {};
 
+  var trigger = '$quotes';
+  
   plugin.help = {
     quotes: '$quotes\n' +
         'Display a random famous quote from the #SexManiac users',
   };
 
   function pubListener(nick, text) {
-    var trigger = '$quotes';
     if (core.util.beginsIgnoreCase(text, trigger)) {
       var arg = text.substring(trigger.length + 1);
-      if (core.util.eqIgnoreCase(text, trigger)) {
-        var total = core.config.quotes.length;
-        var phrase = core.config.quotes[_.random(total - 1)];
-        core.irc.sayPub(phrase);
-      } else if (core.util.beginsIgnoreCase(text, trigger + ' ')) {
-        var quote = core.config.quotes[parseInt(arg)];
-        if (quote) {
-          core.irc.sayPub(quote);
-        } else {
-          core.irc.sayFmt('no quote "%s"', arg);
+      if (arg.match(/^\s*$/)) { //if nothing or any number of whitespaces
+        var selectedIndex = _.random(core.config.quotes.length - 1);
+        var selectedItem = core.config.quotes[selectedIndex];
+        sayQuote(selectedItem);
+      } else {
+        var quoteItem = _.find(core.config.quotes, function(elem) {
+          return elem.key == arg.trim();
+        });
+
+        if(quoteItem === undefined) {
+          core.irc.sayFmt('No quote tagged "%s"', arg);
+        } else {       
+          sayQuote(quoteItem);
         }
       }
     }
   }
+  
+  function sayQuote (quoteObject) {
+    if(quoteObject.hasOwnProperty('quote')) {
+      core.irc.sayFmt('"%s" -%s, %s', quoteObject.quote, quoteObject.author,
+          quoteObject.timestamp);
+    } else if(quoteObject.hasOwnProperty('link')) {
+      core.irc.sayPub(quoteObject.link);
+    } else {
+      core.irc.sayFmt('An entry tagged "%s" exists, but is neither a quote or' +
+          ' a link.', quoteObject.tag);
+    }
+    
+  }
+
   plugin.load = function () {
     core.irc.on('pub', pubListener);
   };
