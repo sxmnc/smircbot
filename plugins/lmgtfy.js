050343b14c7717012f72789ea3439789341be548
Simon Bernier St-Pierre <sbernierstpierre@gmail.com>
2015-07-09 14:12:46 -0400

    Add lmgtfy plugin

---
+++ plugins/lmgtfy.js
@@ -0,0 +1,24 @@
+var request = require('request');
+
+module.exports = function (core) {
+  var plugin = {};
+
+  var regex = /^\$(?:lmgtfy|snob|rtfm)\s(.+)$/;
+
+  function pubListener(nick, text) {
+    var match = text.match(regex);
+    if (match !== null) {
+        core.irc.sayFmt('http://lmgtfy.com/?q=%s', encodeURIComponent(match[1]));
+    }
+  }
+
+  plugin.load = function () {
+    core.irc.on('pub', pubListener);
+  };
+
+  plugin.unload = function () {
+    core.irc.removeListener('pub', pubListener);
+  };
+
+  return plugin;
+};
