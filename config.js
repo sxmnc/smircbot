3816c471993ac00b345d539e4dc2d92c6034c07b
Alexandre Gagnon <alxgnon@gmail.com>
2015-07-11 23:07:53 -0400

    fix formatting of config.js

--- config.js
+++ config.js
@@ -1,113 +1,113 @@
 module.exports = {
-  core: {
-    server: "irc.freenode.net",
-    port: 6667,
-    channel: "#SexManiac",
+    core: {
+        server: "irc.freenode.net",
+        port: 6667,
+        channel: "#SexManiac",
 
-    nickname: "smircbot",
-    realname: "#SexManiac IRC bot",
-    password: "********",
+        nickname: "smircbot",
+        realname: "#SexManiac IRC bot",
+        password: "********",
 
-    debug: true,
-  },
-
-  translateApiKey: "********",
-  weatherApiKey: "********",
-
-  //mysql: {
-  //    connectionLimit: 10,
-  //    host: "localhost",
-  //    user: "root",
-  //    database: "BigDataAnalytics",
-  //},
-
-  typist: [
-    "the quick brown fox jumps over the lazy dog",
-    "moonlight drowns out all but the brightest stars",
-    "I want to be a healer, and love all things that grow and are not barren",
-    "a hunted man sometimes wearies of distrust and longs for friendship",
-    "never tell me the odds!",
-    "you do have your moments. Not many, but you have them",
-    "Ben! I can be a Jedi. Ben, tell him I'm ready!",
-    "I have a bad feeling about this",
-    "I'll never join you",
-    "IT'S A TRAP",
-    "you are a member of the rebel alliance, and a traitor",
-    "it's against my programming to impersonate a deity",
-    "hold on to your butts",
-    "that means they only eat vegetables, but for you, I think they'd make an exception",
-    "God help us, we're in the hands of engineers",
-    "I am not a computer nerd. I prefer to be called a hacker!",
-    "take your stinking paws off me, you damned dirty ape",
-    "I'm going to make him an offer he can't refuse",
-    "Toto, I've got a feeling we're not in Kansas anymore",
-    "I love the smell of napalm in the morning",
-    "Bond. James Bond",
-    "there's no place like home",
-    "you're gonna need a bigger boat",
-    "Houston, we have a problem",
-    "keep your friends close, but your enemies closer",
-    "forget it, Jake, it's Chinatown",
-    "open the pod bay doors please, HAL",
-    "my precious",
-    "I'm the king of the world!",
-  ],
-
-  quotes: [
-    {
-      key: "loli",
-      quote: "\"C'est parce que c'est pas du loli rape\" -kiniamaro 2014",
-    }, {
-      key: "mac",
-      quote: "\"ouais, I'm not letting it die, c'est mon dernier mac à vie\" -alxgnon 2014"
-    }, {
-      key: "arch",
-      quote: "\"Check ben comment j'installe Arch en 30 minutes!\" -MrJudgemental 2014"
-    }, {
-      key: "biblio",
-      quote: "\"En arrière de la biblio municipale\" -SBSTP 2014",
-    }, {
-      key: "broken",
-      quote: "\"The bot is broken!\" -everyone 2014",
-    }, {
-      key: "rodric",
-      quote: "http://i.imgur.com/0CW6W7t.jpg"
-    }, {
-      key: "lookingformaniac",
-      quote: "http://pastebin.com/BrtTPvXT"
-    }, {
-      key: "nonsense",
-      quote: "\"tu me faire un debian pi un avec un pingouin du si vx\" - SBSTP 2014"
-    }, {
-      key: "irc",
-      quote: "\"ça me tente pas d'attendre sur l'irc\" - DrFoliberg 2014"
-    }, {
-      key: "grab",
-      quote: "\"moi j'aime bien grab des petites filles defois\" - alxgnon 2014"
-    }, {
-      key: "java",
-      quote: "\"laissez moi faire du java criss\" - DrFoliberg 2014"
-    }, {
-      key: "drugs",
-      quote: "\"Alright I taking drugs\" - Maro-kun 2015"
-    }, {
-      key: "16",
-      quote: "\"non fadrait que seize ans messemble c'est faisable\" - Maro-kun 2015",
-    }, {
-      key: "cul",
-      quote: "\"Entk, moi j'aime juste les culs de gars.\" - petitprince 2015",
-    }, {
-      key: "lesbien",
-      quote: "\"I NEVER NEVER WAS A lesbien\" - Maro-Kun 2015",
-    }, {
-      key: "<3 java",
-      quote: "\"<3 java\" - maxdup 2015",
-    }, {
-      key: "privé",
-      quote: "\"comment tu as eu ca?? c'est tres privé !!!!\" - danou_ 2015",
-    }, {
-      key: "snake",
-      quote: "\"Dude, I'd fuck that snake raw\" - FifteenIsForShow 2015",
+        debug: true,
     },
-  ],
+
+    translateApiKey: "********",
+    weatherApiKey: "********",
+
+    //mysql: {
+    //    connectionLimit: 10,
+    //    host: "localhost",
+    //    user: "root",
+    //    database: "BigDataAnalytics",
+    //},
+
+    typist: [
+        "the quick brown fox jumps over the lazy dog",
+        "moonlight drowns out all but the brightest stars",
+        "I want to be a healer, and love all things that grow and are not barren",
+        "a hunted man sometimes wearies of distrust and longs for friendship",
+        "never tell me the odds!",
+        "you do have your moments. Not many, but you have them",
+        "Ben! I can be a Jedi. Ben, tell him I'm ready!",
+        "I have a bad feeling about this",
+        "I'll never join you",
+        "IT'S A TRAP",
+        "you are a member of the rebel alliance, and a traitor",
+        "it's against my programming to impersonate a deity",
+        "hold on to your butts",
+        "that means they only eat vegetables, but for you, I think they'd make an exception",
+        "God help us, we're in the hands of engineers",
+        "I am not a computer nerd. I prefer to be called a hacker!",
+        "take your stinking paws off me, you damned dirty ape",
+        "I'm going to make him an offer he can't refuse",
+        "Toto, I've got a feeling we're not in Kansas anymore",
+        "I love the smell of napalm in the morning",
+        "Bond. James Bond",
+        "there's no place like home",
+        "you're gonna need a bigger boat",
+        "Houston, we have a problem",
+        "keep your friends close, but your enemies closer",
+        "forget it, Jake, it's Chinatown",
+        "open the pod bay doors please, HAL",
+        "my precious",
+        "I'm the king of the world!",
+    ],
+
+    quotes: [
+        {
+            key: "loli",
+            quote: "\"C'est parce que c'est pas du loli rape\" -kiniamaro 2014",
+        }, {
+            key: "mac",
+            quote: "\"ouais, I'm not letting it die, c'est mon dernier mac à vie\" -alxgnon 2014",
+        }, {
+            key: "arch",
+            quote: "\"Check ben comment j'installe Arch en 30 minutes!\" -MrJudgemental 2014",
+        }, {
+            key: "biblio",
+            quote: "\"En arrière de la biblio municipale\" -SBSTP 2014",
+        }, {
+            key: "broken",
+            quote: "\"The bot is broken!\" -everyone 2014",
+        }, {
+            key: "rodric",
+            quote: "http://i.imgur.com/0CW6W7t.jpg",
+        }, {
+            key: "lookingformaniac",
+            quote: "http://pastebin.com/BrtTPvXT",
+        }, {
+            key: "nonsense",
+            quote: "\"tu me faire un debian pi un avec un pingouin du si vx\" - SBSTP 2014",
+        }, {
+            key: "irc",
+            quote: "\"ça me tente pas d'attendre sur l'irc\" - DrFoliberg 2014",
+        }, {
+            key: "grab",
+            quote: "\"moi j'aime bien grab des petites filles defois\" - alxgnon 2014",
+        }, {
+            key: "java",
+            quote: "\"laissez moi faire du java criss\" - DrFoliberg 2014",
+        }, {
+            key: "drugs",
+            quote: "\"Alright I taking drugs\" - Maro-kun 2015",
+        }, {
+            key: "16",
+            quote: "\"non fadrait que seize ans messemble c'est faisable\" - Maro-kun 2015",
+        }, {
+            key: "cul",
+            quote: "\"Entk, moi j'aime juste les culs de gars.\" - petitprince 2015",
+        }, {
+            key: "lesbien",
+            quote: "\"I NEVER NEVER WAS A lesbien\" - Maro-Kun 2015",
+        }, {
+            key: "<3 java",
+            quote: "\"<3 java\" - maxdup 2015",
+        }, {
+            key: "privé",
+            quote: "\"comment tu as eu ca?? c'est tres privé !!!!\" - danou_ 2015",
+        }, {
+            key: "snake",
+            quote: "\"Dude, I'd fuck that snake raw\" - FifteenIsForShow 2015",
+        },
+    ],
 };
