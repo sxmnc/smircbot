030c22bfbe289891736cd60777a6c824397e889d
Alexandre Gagnon <alxgnon@gmail.com>
2015-07-12 09:50:30 -0400

    link: extract whatCD function

--- plugins/link.js
+++ plugins/link.js
@@ -4,6 +4,25 @@ var request = require("request");
 module.exports = function (core) {
   var plugin = {};
 
+  function whatCD(r) {
+    r.post({
+      url: "https://what.cd/login.php",
+      followAllRedirects: true,
+      form: {
+        username: core.config.whatcd.username,
+        password: core.config.whatcd.password,
+        login: "Log in",
+      }
+    }, function (err, res, body) {
+      if (!err && res.statusCode === 200) {
+        var result = /<title>\s*(.+)\s*<\/title>/.exec(body);
+        if (result && result.length === 2 && result[1]) {
+          core.irc.sayFmt("link: %s", entities.decodeHTML(result[1]));
+        }
+      }
+    });
+  }
+
   function pubListener(nick, text) {
     var match = text.match(/\bhttps?:\/\/[^\s]+\b/g);
     if (match) {
@@ -14,22 +33,7 @@ module.exports = function (core) {
           var result = /<title>\s*(.+)\s*<\/title>/.exec(body);
           if (result && result.length === 2 && result[1]) {
             if (result[1] === "Login :: What.CD") {
-              r.post({
-                url: "https://what.cd/login.php",
-                followAllRedirects: true,
-                form: {
-                  username: core.config.whatcd.username,
-                  password: core.config.whatcd.password,
-                  login: "Log in",
-                }
-              }, function (err, res, body) {
-                if (!err && res.statusCode === 200) {
-                  var result = /<title>\s*(.+)\s*<\/title>/.exec(body);
-                  if (result && result.length === 2 && result[1]) {
-                    core.irc.sayFmt("link: %s", entities.decodeHTML(result[1]));
-                  }
-                }
-              });
+              whatCD(r);
             } else {
               core.irc.sayFmt("link: %s", entities.decodeHTML(result[1]));
             }
